6cacdad619ea2bd8c245ba7849dea0d1ccb767a2
Harlen Bains <himalayanelixir@gmail.com>
2019-09-23 20:13:47 -0700

    Updating Pi Code to handle multiple Arduinos
    - Still need to implement listening. Right now the code just opens and closes the ports needed to communicate with the boards.

--- raspberry_pi_controller/contoller.py
+++ raspberry_pi_controller/contoller.py
@@ -4,40 +4,7 @@
 
 #=====================================
 
-def sendToArduino(sendStr):
-  ser.write(sendStr.encode())
-
-
-#======================================
-
-def recvFromArduino():
-  global startMarker, endMarker
-  
-  ck = ""
-  x = "z" # any value that is not an end- or startMarker
-  #byteCount = -1 # to allow for the fact that the last increment will be one too many
-  x = ser.read()
-  x = x.decode("utf-8")
-  #print(x)
-  # wait for the start character
-  while  ord(x) != startMarker: 
-    x = ser.read()
-    x = x.decode("utf-8") 
-    #print(x)
-  # save data until the end marker is found
-  while ord(x) != endMarker:
-    if ord(x) != startMarker:
-      ck = ck + x 
-      #byteCount += 1
-    x = ser.read()
-    x = x.decode("utf-8") 
-    #print(x) 
-  return(ck)
-  
-
-#============================
-
-def waitForArduino():
+def waitForArduino(port):
 
    # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
    # it also ensures that any bytes left over from a previous message are discarded
@@ -46,15 +13,42 @@ def waitForArduino():
     
     msg = ""
     while msg.find("Arduino is ready") == -1:
-
-      while ser.inWaiting() == 0:
+      while ser[port].inWaiting() == 0:
         pass
-        
-      msg = recvFromArduino()
+      msg = recvFromArduino(port)
+      print ("Arduino Number:", port, "Is Ready")
+      print ("Message from Arduino:" + msg)
+#======================================
 
-      print (msg)
-      print ("")
-      
+def recvFromArduino(port):
+  global startMarker, endMarker
+  
+  ck = ""
+  x = "z" # any value that is not an end- or startMarker
+  #byteCount = -1 # to allow for the fact that the last increment will be one too many
+  x = ser[port].read()
+  x = x.decode("utf-8")
+  #print(x)
+  # wait for the start character
+  while  ord(x) != startMarker: 
+    x = ser[port].read()
+    x = x.decode("utf-8") 
+    #print(x)
+  # save data until the end marker is found
+  while ord(x) != endMarker:
+    if ord(x) != startMarker:
+      ck = ck + x 
+      #byteCount += 1
+    x = ser[port].read()
+    x = x.decode("utf-8") 
+    #print(x) 
+  return(ck)
+  
+#=====================================
+
+def sendToArduino(sendStr):
+  ser.write(sendStr.encode())
+
 #======================================
 
 def runTest(td):
@@ -87,27 +81,37 @@ import serial
 import time
 import sys
 
-print ("")
-print ("")
 
-serPort = "/dev/cu.SLAB_USBtoUART"
-#serPort = "/dev/ttyUSB0"
-baudRate = 9600
-ser = serial.Serial(serPort, baudRate)
-print ("Serial port " + serPort + " opened  Baudrate " + str(baudRate))
-
+NUMBER_OF_SLAVES = 2
 startMarker = 60
 endMarker = 62
+baudRate = 9600
+serPort = ["/dev/cu.usbmodem1412101", "/dev/cu.usbmodem1412201"]
+#serPort = "/dev/cu.SLAB_USBtoUART"
+#serPort = "/dev/ttyUSB0"
 
-waitForArduino()
+ser = [None] * NUMBER_OF_SLAVES
 
-while 1 :
-    print ("===========")
-    print ("")
-    text = input("Up or Down?: ")
-    text = "<" + text + ">"
-    runTest(text)
-    time.sleep(1)
+for x in range(len(serPort)):
+  ser[x] = serial.Serial(serPort[x], baudRate)
+  # print(ser[x])
+  print ("Serial port " + serPort[x] + " opened")
 
-ser.close
+# Better Printout
+print("")
+
+for port in range(len(serPort)):
+  waitForArduino(port)
+
+#while 1 :
+#    print ("===========")
+#    print ("")
+#    text = input("Up or Down?: ")
+#    text = "<" + text + ">"
+#    runTest(text)
+#    time.sleep(1)
+
+for x in range(len(serPort)):
+  print ("Serial port " + serPort[x] + " closed")
+  ser[x].close
 
