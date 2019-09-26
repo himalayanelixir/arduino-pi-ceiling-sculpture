c96833558c6bb6c45be101bc6bf61c9c79a3d7f8
Harlen Bains <himalayanelixir@gmail.com>
2019-09-26 00:50:06 -0700

    Python Formatting

--- raspberry_pi_controller/contoller.py
+++ raspberry_pi_controller/contoller.py
@@ -1,77 +1,83 @@
-#=====================================
+# =====================================
 
 #  Function Definitions
 
-#=====================================
+# =====================================
+
 
 def waitForArduino(port):
 
-   # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
-   # it also ensures that any bytes left over from a previous message are discarded
-   
+    # wait until the Arduino sends 'Arduino Ready' - allows time for Arduino reset
+    # it also ensures that any bytes left over from a previous message are discarded
+
     global startMarker, endMarker
-    
+
     msg = ""
     while msg.find("Arduino is ready") == -1:
-      while ser[port].inWaiting() == 0:
-        pass
-      msg = recvFromArduino(port)
-      print ("Arduino Number:", port, "Is Ready")
-      print ("Message from Arduino:" + msg)
-#======================================
+        while ser[port].inWaiting() == 0:
+            pass
+        msg = recvFromArduino(port)
+        print("Arduino Number:", port, "Is Ready")
+        print("Message from Arduino:" + msg)
+
+
+# ======================================
+
 
 def recvFromArduino(port):
-  global startMarker, endMarker
-  
-  ck = ""
-  x = "z" # any value that is not an end- or startMarker
-  #byteCount = -1 # to allow for the fact that the last increment will be one too many
-  x = ser[port].read()
-  x = x.decode("utf-8")
-  #print(x)
-  # wait for the start character
-  while  ord(x) != startMarker: 
+    global startMarker, endMarker
+
+    ck = ""
+    x = "z"  # any value that is not an end- or startMarker
+    # byteCount = -1 # to allow for the fact that the last increment will be one too many
     x = ser[port].read()
-    x = x.decode("utf-8") 
-    #print(x)
-  # save data until the end marker is found
-  while ord(x) != endMarker:
-    if ord(x) != startMarker:
-      ck = ck + x 
-      #byteCount += 1
-    x = ser[port].read()
-    x = x.decode("utf-8") 
-    #print(x) 
-  return(ck)
+    x = x.decode("utf-8")
+    # print(x)
+    # wait for the start character
+    while ord(x) != startMarker:
+        x = ser[port].read()
+        x = x.decode("utf-8")
+        # print(x)
+    # save data until the end marker is found
+    while ord(x) != endMarker:
+        if ord(x) != startMarker:
+            ck = ck + x
+            # byteCount += 1
+        x = ser[port].read()
+        x = x.decode("utf-8")
+        # print(x)
+    return ck
 
-#======================================
 
-def run(td,port):
-  waitingForReply = False
+# ======================================
 
-  if waitingForReply == False:
-    ser[port].write(td.encode())
-    print ("-> -> -> -> -> ->")
-    print ("Thread: " + str(port) + " started")
-    print ("Message Sent:")
-    print ("PC: " + td)
-    waitingForReply = True
 
-  if waitingForReply == True:
-
-    while ser[port].inWaiting() == 0:
-      pass
-      
-    dataRecvd = recvFromArduino(port)
-    print ("<- <- <- <- <- <-")
-    print ("Thread: " + str(port) + " complete")
-    print ("Message Received:  " + dataRecvd)
+def run(td, port):
     waitingForReply = False
-    time.sleep(.1)
+
+    if waitingForReply == False:
+        ser[port].write(td.encode())
+        print("-> -> -> -> -> ->")
+        print("Thread: " + str(port) + " started")
+        print("Message Sent:")
+        print("PC: " + td)
+        waitingForReply = True
+
+    if waitingForReply == True:
+
+        while ser[port].inWaiting() == 0:
+            pass
+
+        dataRecvd = recvFromArduino(port)
+        print("<- <- <- <- <- <-")
+        print("Thread: " + str(port) + " complete")
+        print("Message Received:  " + dataRecvd)
+        waitingForReply = False
+        time.sleep(0.1)
 
 
-#======================================
-#======================================
+# ======================================
+# ======================================
 
 import serial
 import time
@@ -91,35 +97,35 @@ ser = [None] * NUMBER_OF_SLAVES
 threads = [None] * NUMBER_OF_SLAVES
 
 for x in range(len(serPort)):
-  ser[x] = serial.Serial(serPort[x], baudRate)
-  print ("Serial port " + serPort[x] + " opened")
+    ser[x] = serial.Serial(serPort[x], baudRate)
+    print("Serial port " + serPort[x] + " opened")
 
 print("")
 
 for port in range(len(serPort)):
-  waitForArduino(port)
+    waitForArduino(port)
 
-while 1 :
-    print ("===========")
-    print ("")
+while 1:
+    print("===========")
+    print("")
     text = input("Enter Commands: ")
-    parse_text = text.split(';')
-    print (parse_text)
-    
+    parse_text = text.split(";")
+    print(parse_text)
+
     # create threads
     for x in range(len(parse_text)):
-      threads[x] = Thread(target=run, args=(parse_text[x],x))
+        threads[x] = Thread(target=run, args=(parse_text[x], x))
 
     # start threads
     for x in range(len(parse_text)):
-      threads[x].start()
+        threads[x].start()
 
     # wait for threads to finish
     for x in range(len(parse_text)):
-      threads[x].join()
+        threads[x].join()
 
-    
+
 # close all serial connections
 for x in range(len(serPort)):
-  print ("Serial port " + serPort[x] + " closed")
-  ser[x].close
+    print("Serial port " + serPort[x] + " closed")
+    ser[x].close
