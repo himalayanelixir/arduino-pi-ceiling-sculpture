722ce2f9e4c98d9da40415cdf7d046a38874aa86
Harlen Bains <himalayanelixir@gmail.com>
2019-09-26 01:03:10 -0700

    Formatting Arduino Code

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -27,7 +27,7 @@ bool new_data = false;
 Servo my_servo[NUMBER_MOTORS];
 // create a array of ports with the order: motor, counter, reset
 // int ports[NUMBER_MOTORS][3] = {{62, 66, 67}};
-int ports[NUMBER_MOTORS][3] = {{2, 3, 4},{5, 6, 7},{8, 9, 10}};
+int ports[NUMBER_MOTORS][3] = {{2, 3, 4}, {5, 6, 7}, {8, 9, 10}};
 // integer array that contains the direction and number of rotations a motor
 // needs to go
 int motor_commands[NUMBER_MOTORS][2] = {0};
