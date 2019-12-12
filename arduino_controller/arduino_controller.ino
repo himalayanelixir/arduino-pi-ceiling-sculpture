fa4e7800003511f6548652d66b3b86ad03637852
Harlen Bains <himalayanelixir@gmail.com>
2019-12-11 16:13:22 -0800

    Added ports for test harness

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -5,7 +5,7 @@
 #define DEBOUNCE_TIME .4
 #define SAMPLE_FREQUENCY 20
 #define MAXIMUM (DEBOUNCE_TIME * SAMPLE_FREQUENCY)
-#define NUMBER_MOTORS 2
+#define NUMBER_MOTORS 4
 #define TIMEOUT 10000
 
 // function declarations`
@@ -23,7 +23,7 @@ bool new_data = false;
 Servo my_servo[NUMBER_MOTORS];
 // create a array of ports with the order: motor, counter, reset
 // int ports[NUMBER_MOTORS][3] = {{62, 66, 67}};
-int ports[NUMBER_MOTORS][3] = {{2, 3, 4}, {5, 6, 7}};
+int ports[NUMBER_MOTORS][3] = {{11, 12, 13}, {8, 9, 10}, {5, 6, 7}, {2, 3, 4}};
 // integer array that contains the direction and number of rotations a motor
 // needs to go
 int motor_commands[NUMBER_MOTORS][2] = {0};
