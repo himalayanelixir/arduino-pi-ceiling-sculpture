0102f3a125ecb760be4f053f160ec4e5ba3d362f
Harlen Bains <himalayanelixir@gmail.com>
2020-01-31 10:40:53 -0800

    Increased Timeout

--- arduino_controller/arduino_controller.ino
+++ arduino_controller/arduino_controller.ino
@@ -5,9 +5,9 @@
 #define DEBOUNCE_TIME .4
 #define SAMPLE_FREQUENCY 20
 #define MAXIMUM (DEBOUNCE_TIME * SAMPLE_FREQUENCY)
-#define NUMBER_MOTORS 1
-#define NUMBER_MOTORS_MOVING 1
-#define TIMEOUT 10000
+#define NUMBER_MOTORS 11
+#define NUMBER_MOTORS_MOVING 3
+#define TIMEOUT 20000
 
 // function declarations
 void RecvWithStartEndMarkers();
@@ -28,7 +28,7 @@ bool new_data = false;
 // create servo objects
 Servo my_servo[NUMBER_MOTORS];
 // create a array of ports with the order: motor, counter, reset
-int ports[NUMBER_MOTORS][3] = {{62, 66, 67}};
+int ports[NUMBER_MOTORS][3] = {{11, 12, 13}, {8, 9, 10}, {5, 6, 7}, {2, 3, 4}, {14, 15, 16}, {17, 18, 19}, {20, 21, 22}, {23, 24, 25}, {29, 30, 31}, {35, 36, 37}, {41, 42, 43}};
 // integer array that contains the direction and number of rotations a motor, and a flag that determines if it's moving
 int motor_commands[NUMBER_MOTORS][3] = {0};
 // array of new switch values
