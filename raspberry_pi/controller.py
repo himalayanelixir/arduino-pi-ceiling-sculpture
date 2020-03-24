acbd7f71b9275b6cfe9e9fd2973b50e9d3af79ca
Harlen Bains <himalayanelixir@gmail.com>
2020-03-23 17:02:47 -0700

    Issue #6 controller.py doesn't use csvs (#12)
    * added extra options for csv execution
    
    * moved files around
    
    * finished linting function
    
    * removed extra import
    
    * controller.py got getCommands() working
    
    * added reset and exeute functions to controller.py

--- raspberry_pi/controller.py
+++ raspberry_pi/controller.py
@@ -3,10 +3,12 @@
 import serial
 import time
 import sys
+import shutil
 from threading import Thread
 from yaspin import yaspin
 from yaspin.spinners import Spinners
 import timeout_decorator
+import csv
 
 
 def waitForArduino(port):
@@ -80,6 +82,90 @@ def runExecute(td, port):
         waitingForReply = False
 
 
+def lintDesiredState():
+    # Note here we are using commands_state.csv as a buffer as we lint desired_state.csv
+    # Didn't want to have an extra file just to store the temp values
+    desiredStateList = []
+    with open("desired_state.csv", "r") as desiredStateFile:
+        desiredStateReader = csv.reader(desiredStateFile, delimiter=",")
+        desiredStateList = list(desiredStateReader)
+    for countRow,row in enumerate(desiredStateList):
+        for countColumn, column in enumerate(row):
+            if int(column) > MAX_TURNS:
+                desiredStateList[countRow][countColumn] = str(MAX_TURNS)
+            elif int(column) < 0:
+                desiredStateList[countRow][countColumn] = "0"
+    with open("desired_state.csv", "w", newline="") as desiredStateFile:
+        desiredStateWriter = csv.writer(desiredStateFile, quoting=csv.QUOTE_ALL)
+        desiredStateWriter.writerows(desiredStateList)
+
+
+def getCommands():
+    global commandString
+    commandString = ""
+    desiredStateList = []
+    currentStateList = []
+    with open("desired_state.csv", "r") as desiredStateFile:
+        desiredStateReader = csv.reader(desiredStateFile, delimiter=",")
+        desiredStateList = list(desiredStateReader)
+    with open("current_state.csv", "r", newline='') as currentStateFile:
+        currentstateReader = csv.reader(currentStateFile, delimiter=",")
+        currentStateList = list(currentstateReader)
+    # assumption here is that both csvs are the size
+    for countRow,row in enumerate(desiredStateList):
+        commandString += "<"
+        for countColumn, column in enumerate(row):
+            difference = int(currentStateList[countRow][countColumn]) - int(desiredStateList[countRow][countColumn])
+            if difference < 0:
+                commandString += "Down,"
+            elif difference > 0:
+                commandString += "Up,"
+            else:
+                commandString += "None,"
+            commandString += str(abs(difference)) + ","
+        commandString = commandString[:-1]
+        commandString += ">;"
+    # remove final semicolon
+    commandString = commandString[:-1]
+
+def getResetCommands():
+    global commandString
+    currentStateList = []
+    commandString = ""
+    print(commandString)
+    with open("current_state.csv", "r") as currentStateFile:
+        currentStateReader = csv.reader(currentStateFile, delimiter=",")
+        currentStateList = list(currentStateReader)
+    for countRow,row in enumerate(currentStateList):
+        commandString += "<"
+        for countColumn, column in enumerate(row):
+            currentStateList[countRow][countColumn] = "0"
+            commandString += "Up,100,"
+        commandString = commandString[:-1]
+        commandString += ">;"
+    commandString = commandString[:-1]
+    with open("current_state.csv", "w", newline="") as currentStateFile:
+        currentStateWriter = csv.writer(currentStateFile, quoting=csv.QUOTE_ALL)
+        currentStateWriter.writerows(currentStateList) 
+    print(commandString)
+
+def executeCommands():
+    parse_text = commandString.split(";")
+    spinner.start()
+    # create threads
+    for x in range(len(parse_text)):
+        threads[x] = Thread(target=run, args=(parse_text[x], x))
+    # start threads
+    for x in range(len(parse_text)):
+        threads[x].start()
+    # wait for threads to finish
+    for x in range(len(parse_text)):
+        threads[x].join()
+    spinner.stop()
+
+def updateCurrentState():
+    shutil.copy2('desired_state.csv"','current_state.csv')
+
 def errorCheck():
     if didErrorOccur == True:
         closeConnections()
@@ -119,7 +205,7 @@ def closeConnections():
 
 
 # global variables
-NUMBER_OF_SLAVES = 2
+NUMBER_OF_ARRAYS = 2
 startMarker = 60
 endMarker = 62
 baudRate = 9600
@@ -128,9 +214,16 @@ spinner = yaspin(Spinners.weather)
 didErrorOccur = False
 
 # initialize serial variable array
-ser = [None] * NUMBER_OF_SLAVES
-threads = [None] * NUMBER_OF_SLAVES
-connectingThreads = [None] * NUMBER_OF_SLAVES
+ser = [None] * NUMBER_OF_ARRAYS
+threads = [None] * NUMBER_OF_ARRAYS
+connectingThreads = [None] * NUMBER_OF_ARRAYS
+
+
+#TODO: See if we actually use these
+NUMBER_OF_MOTORS_IN_ARRAY = 22
+MAX_TURNS = 10
+commandString = ""
+parse_text = ""
 
 while True:
     didErrorOccur == False
@@ -189,20 +282,30 @@ while True:
 
 while inputText1 != "Exit" and inputText1 != "exit":
     print("===========\n")
-    inputText2 = input("Enter Commands ('Exit' to close program): ")
-    if inputText2 == "Exit" or inputText2 == "exit":
+    inputText2 = input("Enter '1' to set ceiling from csv, '2' to reset, '3' for manual mode, and 'Exit' to close program)\n : ")
+    # csv mode
+    if inputText2 == "1":
+        print("CSV Mode\n")
+        lintDesiredState()
+        getCommands()
+        executeCommands()
+        # updateCurrentState()
+    # csv reset
+    elif(inputText2 == "2"):
+        print("CSV Reset Mode\n")
+        getResetCommands()
+        executeCommands()
+    # manual mode
+    elif(inputText2 == "3"):
+        commandString = ""
+        commandString = input("Enter Commands (format '<Up,1>;<Up,1>'):\n : ")
+        executeCommands()
+    # exit
+    elif(inputText2 == "Exit" or inputText2 == "exit"):
         # close all serial connections
         closeConnections()
         break
-    parse_text = inputText2.split(";")
-    spinner.start()
-    # create threads
-    for x in range(len(parse_text)):
-        threads[x] = Thread(target=run, args=(parse_text[x], x))
-    # start threads
-    for x in range(len(parse_text)):
-        threads[x].start()
-    # wait for threads to finish
-    for x in range(len(parse_text)):
-        threads[x].join()
-    spinner.stop()
+    else:
+        print("Invalid Input\n")
+
+
