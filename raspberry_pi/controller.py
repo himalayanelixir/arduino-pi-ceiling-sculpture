a3e641251652f9a12fc161c38724295c2764f19f
Harlen Bains <himalayanelixir@gmail.com>
2020-03-27 15:24:50 -0700

    Combined functions

--- raspberry_pi/controller.py
+++ raspberry_pi/controller.py
@@ -84,14 +84,15 @@ def runExecute(td, port):
         waitingForReply = False
 
 
-def lintDesiredState():
-    # Note here we are using commands_state.csv as a buffer as we lint desired_state.csv
-    # Didn't want to have an extra file just to store the temp values
+def csvCommands():
+    global commandString
+
+    # lint desired_state csv values and make sure that they are in range
     desiredStateList = []
     with open("desired_state.csv", "r") as desiredStateFile:
         desiredStateReader = csv.reader(desiredStateFile, delimiter=",")
         desiredStateList = list(desiredStateReader)
-    for countRow,row in enumerate(desiredStateList):
+    for countRow, row in enumerate(desiredStateList):
         for countColumn, column in enumerate(row):
             if int(column) > MAX_TURNS:
                 desiredStateList[countRow][countColumn] = str(MAX_TURNS)
@@ -101,23 +102,23 @@ def lintDesiredState():
         desiredStateWriter = csv.writer(desiredStateFile, quoting=csv.QUOTE_ALL)
         desiredStateWriter.writerows(desiredStateList)
 
-
-def getCommands():
-    global commandString
+    # fill command string
     commandString = ""
     desiredStateList = []
     currentStateList = []
     with open("desired_state.csv", "r") as desiredStateFile:
         desiredStateReader = csv.reader(desiredStateFile, delimiter=",")
         desiredStateList = list(desiredStateReader)
-    with open("current_state.csv", "r", newline='') as currentStateFile:
+    with open("current_state.csv", "r", newline="") as currentStateFile:
         currentstateReader = csv.reader(currentStateFile, delimiter=",")
         currentStateList = list(currentstateReader)
     # assumption here is that both csvs are the size
-    for countRow,row in enumerate(desiredStateList):
+    for countRow, row in enumerate(desiredStateList):
         commandString += "<"
         for countColumn, column in enumerate(row):
-            difference = int(currentStateList[countRow][countColumn]) - int(desiredStateList[countRow][countColumn])
+            difference = int(currentStateList[countRow][countColumn]) - int(
+                desiredStateList[countRow][countColumn]
+            )
             if difference < 0:
                 commandString += "Down,"
             elif difference > 0:
@@ -129,15 +130,20 @@ def getCommands():
         commandString += ">;"
     # remove final semicolon
     commandString = commandString[:-1]
+    # call execute commands
+    executeCommands()
+    # update current_state.csv with the values of desired_state.csv
+    shutil.copy2('desired_state.csv"', "current_state.csv")
 
-def getResetCommands():
+
+def resetCommands():
     global commandString
     currentStateList = []
     commandString = ""
     with open("current_state.csv", "r") as currentStateFile:
         currentStateReader = csv.reader(currentStateFile, delimiter=",")
         currentStateList = list(currentStateReader)
-    for countRow,row in enumerate(currentStateList):
+    for countRow, row in enumerate(currentStateList):
         commandString += "<"
         for countColumn, column in enumerate(row):
             currentStateList[countRow][countColumn] = "0"
@@ -147,7 +153,10 @@ def getResetCommands():
     commandString = commandString[:-1]
     with open("current_state.csv", "w", newline="") as currentStateFile:
         currentStateWriter = csv.writer(currentStateFile, quoting=csv.QUOTE_ALL)
-        currentStateWriter.writerows(currentStateList) 
+        currentStateWriter.writerows(currentStateList)
+    # call execute commands
+    executeCommands()
+
 
 def executeCommands():
     parse_text = commandString.split(";")
@@ -163,8 +172,6 @@ def executeCommands():
         threads[x].join()
     spinner.stop()
 
-def updateCurrentState():
-    shutil.copy2('desired_state.csv"','current_state.csv')
 
 def errorCheck():
     if didErrorOccur == True:
@@ -219,7 +226,7 @@ threads = [None] * NUMBER_OF_ARRAYS
 connectingThreads = [None] * NUMBER_OF_ARRAYS
 
 
-#TODO: See if we actually use these
+# TODO: See if we actually use these
 NUMBER_OF_MOTORS_IN_ARRAY = 22
 MAX_TURNS = 10
 commandString = ""
@@ -297,31 +304,27 @@ while True:
 
 while inputText1 != "Exit" and inputText1 != "exit":
     print("===========\n")
-    inputText2 = input("Enter '1' to set ceiling from csv, '2' to reset, '3' for manual mode, and 'Exit' to close program)\n : ")
+    inputText2 = input(
+        "Enter '1' to set ceiling from csv, '2' to reset, '3' for manual mode, and 'Exit' to close program)\n : "
+    )
     # csv mode
     if inputText2 == "1":
         print("CSV Mode\n")
-        lintDesiredState()
-        getCommands()
-        executeCommands()
-        # updateCurrentState()
+        csvCommands()
     # csv reset
-    elif(inputText2 == "2"):
+    elif inputText2 == "2":
         print("CSV Reset Mode\n")
-        getResetCommands()
-        executeCommands()
+        resetCommands()
     # manual mode
-    elif(inputText2 == "3"):
+    elif inputText2 == "3":
         print("Manual Mode\n")
         commandString = ""
         commandString = input("Enter Commands (format '<Up,1>;<Up,1>'):\n : ")
         executeCommands()
     # exit
-    elif(inputText2 == "Exit" or inputText2 == "exit"):
+    elif inputText2 == "Exit" or inputText2 == "exit":
         # close all serial connections
         closeConnections()
         break
     else:
         print("Invalid Input\n")
-
-
