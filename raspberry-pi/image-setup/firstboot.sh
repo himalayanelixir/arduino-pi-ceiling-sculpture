8a326240fe6f142e173b382676702a5eff0ad662
Harlen Bains <himalayanelixir@gmail.com>
2020-05-10 15:25:29 -0700

    updated firstboot.sh

--- raspberry-pi/image-setup/firstboot.sh
+++ raspberry-pi/image-setup/firstboot.sh
@@ -16,26 +16,22 @@ sudo -u pi sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh
 # disable ohmyzsh auto update
 sed -i 's/# DISABLE_AUTO_UPDATE="true"/DISABLE_AUTO_UPDATE="true"/g' /home/pi/.zshrc
 
-# create virtual environment for controller 
+# create virtual environment for python scripts 
 python3 -m venv /home/pi/pi_env
-# download the controller program from github with it's requirements.txt
-wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/controller.py
+# download the requirements.txt from Github
 wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/requirements.txt
 # install pip dependencies from requirements.txt in the virtual environment
 source /home/pi/pi_env/bin/activate
 pip3 install -r /home/pi/requirements.txt
 deactivate
-# make controller program executable
-chmod +x /home/pi/controller.py
-# add controller program to PATH
-echo "export PATH=\"/home/pi:$PATH\"" >>/home/pi/.zshrc
-# set so that the controller starts up when a user logs in a virtual environment
-echo "controller.py" >>/home/pi/.zshrc
+# remove requirements.txt
+rm /home/pi/requirements.txt
+
+# download the controller program from github with it's requirements.txt
+wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/controller.py
 # download state csvs
 wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/current-state.csv
 wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/desired-state.csv
-# remove requirements.txt
-rm /home/pi/requirements.txt
 
 # download leds program from github
 wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/leds.py
@@ -46,12 +42,11 @@ After=network.target
 
 [Service]
 User=pi
-ExecStart=/home/pi/leds.py 
+ExecStart=/home/pi/leds.py
 
 [Install]
 WantedBy=multi-user.target
 EOT
-chmod +x /home/pi/leds.py 
 sudo systemctl enable leds.service
 
 # download button program from github
@@ -63,32 +58,39 @@ After=network.target
 
 [Service]
 User=pi
-ExecStart=/home/pi/button.py 
+ExecStart=/home/pi/button.py
 
 [Install]
 WantedBy=multi-user.target
 EOT
-chmod +x /home/pi/button.py 
 sudo systemctl enable button.service
 
 # download restart program from github
 wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/restart.py
-chmod +x /home/pi/restart.py
 
 # download shutdown program from github
 wget -P /home/pi https://raw.githubusercontent.com/himalayanelixir/arduino-pi-ceiling-sculpture/master/raspberry-pi/shutdown.py
-chmod +x /home/pi/shutdown.py
 
-# make pi user owner of all the files we downloaded
+# make all the files we downloaded executable
+chmod +x /home/pi/controller.py /home/pi/leds.py /home/pi/button.py /home/pi/shutdown.py /home/pi/restart.py
+# make pi user owner of all the files we downloaded (needed since this program runs as root on firstboot)
 chown -R pi /home/pi
+# setup aliases
+echo 'alias controller="/home/pi/controller.py"' >>/home/pi/.zshrc
+echo 'alias shutdown="/home/pi/shutdown.py"' >>/home/pi/.zshrc
+echo 'alias restart="/home/pi/restart.py"' >>/home/pi/.zshrc
+# add /home/pi to path
+echo "export PATH=\"/home/pi:$PATH\"" >>/home/pi/.zshrc
+# set so that the controller starts up when a user logs in a virtual environment
+echo "controller.py" >>/home/pi/.zshrc
 
 # block all internet access other than incomming ssh from local network
-# outgoing isn't blocked by default, we don't want updates unless we explicitly disable the firewall 
+# outgoing isn't blocked by default, we don't want updates unless we explicitly disable the firewall
 ufw default deny outgoing
 # allow local ssh
 ufw allow from 192.168.1.0/24 to any port 22
 # enable ufw, will auto start on boot
 echo "y" | ufw enable
 
-# tell pi to restart after one minute. This is needed for the ssh changes to work and for the adafruit screen drivers
+# tell pi to restart after one minute
 shutdown -r 1
