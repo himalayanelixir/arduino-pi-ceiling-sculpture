5219b4d31728192f9e59f808be111e503ee61d7e
Harlen Bains <himalayanelixir@gmail.com>
2020-05-22 21:56:06 -0700

    added menus to shutdown and restart programs

--- raspberry-pi/restart.py
+++ raspberry-pi/restart.py
@@ -6,13 +6,40 @@
   from button.py and blinks the button leds 3 times before restarting.
 """
 
+import os
+import RPi.GPIO as GPIO  # pylint: disable=import-error
+import questionary  # pylint: disable=import-error
 from button import restart
 
 
 def main():
     """Restarts the Raspberry Pi
     """
-    restart(23)
+    print(
+        """\033[96m                   __             __ """
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """   ________  _____/ /_____ ______/ /_"""
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """  / ___/ _ \/ ___/ __/ __ `/ ___/ __/"""
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """ / /  /  __(__  ) /_/ /_/ / /  / /_  """
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """/_/   \___/____/\__/\__,_/_/   \__/  \033[0m"""
+    )  # pylint: disable=anomalous-backslash-in-string
+    input_text = questionary.select(
+        "Do you want to restart the Raspberry Pi?", choices=["Yes", "No"]
+    ).ask()
+    if input_text == "Yes":
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        led_pin = 23
+        GPIO.setup(led_pin, GPIO.OUT)
+        os.system("sudo systemctl stop button.service")
+        restart(led_pin)
 
 
 if __name__ == "__main__":
--- raspberry-pi/shutdown.py
+++ raspberry-pi/shutdown.py
@@ -6,13 +6,40 @@
   from button.py and blinks the button leds 5 times before shutting down.
 """
 
+import os
+import RPi.GPIO as GPIO  # pylint: disable=import-error
+import questionary  # pylint: disable=import-error
 from button import shutdown
 
 
 def main():
     """Shutsdown the Raspberry Pi
     """
-    shutdown(23)
+    print(
+        """\033[96m         __          __      __                  """
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """   _____/ /_  __  __/ /_____/ /___ _      ______ """
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """  / ___/ __ \/ / / / __/ __  / __ \ | /| / / __ \ """
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """ (__  ) / / / /_/ / /_/ /_/ / /_/ / |/ |/ / / / /"""
+    )  # pylint: disable=anomalous-backslash-in-string
+    print(
+        """/____/_/ /_/\__,_/\__/\__,_/\____/|__/|__/_/ /_/ \033[0m"""
+    )  # pylint: disable=anomalous-backslash-in-string
+    input_text = questionary.select(
+        "Do you want to shutdown the Raspberry Pi?", choices=["Yes", "No"]
+    ).ask()
+    if input_text == "Yes":
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        led_pin = 23
+        GPIO.setup(led_pin, GPIO.OUT)
+        os.system("sudo systemctl stop button.service")
+        shutdown(led_pin)
 
 
 if __name__ == "__main__":
